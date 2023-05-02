0d6c9b75849e6ccce1f1c059f253050d444fa1fe
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-05-02 18:53:15 +0100

    test: test_feedback initial commit

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
-"""Tests for package cookietest.py
-To use tests either:
-    1 - Use pip to install package as "editable"
-            pip install -e .
-    2 - Import pathmagic.py to enable tests to find the package
-"""
+# Core Library modules
+
 # Third party modules
+import pytest
 
 # First party modules
 from pynamer import pynamer
+
+
+@pytest.mark.parametrize(
+    "message, message_type, result",
+    [
+        ("this is a null message", "null", "None"),
+        ("this is a nominal message", "nominal", "None"),
+        ("this is a warning message", "warning", "None"),
+        ("this is an error message", "error", "None"),
+        ("this is an error message", "wrong", "None"),
+    ],
+)
+def test_feedback(message, message_type, result):
+    """Pytest test to assert mark parametrize pytest feature."""
+    assert result == str(pynamer._feedback(message, message_type))

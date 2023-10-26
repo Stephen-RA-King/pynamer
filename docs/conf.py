07494517e0ba9144e371fa9748d1eb054b7618b0
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-10-26 20:44:47 +0100

    docs: update requirements

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,6 @@ author = "Stephen R A King"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",

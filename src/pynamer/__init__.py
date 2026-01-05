3f07d6aa34d3df9279ea81735dd5f55e1f546e2c
Jinja Genie <JinjaGenie@github.com>
2026-01-05 02:03:07 +0000

    Jinja2 template successfully applied

--- src/pynamer/__init__.py
+++ src/pynamer/__init__.py
@@ -67,7 +67,7 @@ else:  # pragma: no cover
 if project_count_file_trv.is_file():
     project_count = pickle.loads(project_count_file_trv.read_bytes())
 else:  # pragma: no cover
-    project_count = 720293
+    project_count = 720657
 
 
 if meta_file_trv.is_file():

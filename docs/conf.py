40ef7ff035039f975379f927004ffd8d19bca48c
semantic-release <semantic-release>
2026-05-19 19:26:57 +0100

    2.2.0
    Automatically generated by python-semantic-release

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "pynamer"
-version = "2.1.8"
+version = "2.2.0"
 copyright = "2023, Stephen R A King"
 author = "Stephen R A King"
 
